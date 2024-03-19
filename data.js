66f8ab8aa837a525ed9ff0c7fab66c105e5e299d
zemanar <158493347+zemanar@users.noreply.github.com>
2024-03-19 20:44:10 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,14 @@
+function convertNames(inputElement, outputElement) {
+        fetch("https://raw.githubusercontent.com/zemanar/EntoMM/main/data.json")
+          .then((res) => res.json())
+          .then((data) => {
+            const inputName = inputElement.value.trim().toLowerCase();
+            const fullName = inputName.split(" ");
+            const convertedNames = fullName.map((w) => (data[w] ? data[w] : ""));
+            const fullConvertedName = convertedNames.join("");
+            outputElement.value = fullConvertedName;
+          })
+          .catch((error) => {
+            console.error("Error fetching data:", error);
+          });
+      }
